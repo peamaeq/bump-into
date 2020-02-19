2efc6037364c2614a14305a24fc980b1e3a73637
Autumn <autumnontape@tutanota.com>
2020-02-19 13:11:27 -0800

    add a README

---
+++ README.md
@@ -0,0 +1,33 @@
+# bump-into
+
+A `no_std` bump allocator over an arbitrary region of memory. Can
+be used to pass objects up the stack when it would otherwise be
+inconvenient; for instance, if the objects are dynamically sized.
+
+## Example
+
+```rust
+use bump_into::{self, BumpInto};
+
+// allocate 64 bytes of uninitialized space on the stack
+let mut bump_into_space = bump_into::space!(64);
+let bump_into = BumpInto::new(&mut bump_into_space[..]);
+
+// allocating an object produces a mutable reference with
+// the same lifetime as the `BumpInto` instance, or `None`
+// if there isn't enough space
+let number: &mut u64 = bump_into
+    .alloc_with(|| 123)
+    .expect("not enough space");
+assert_eq!(*number, 123);
+*number = 50000;
+assert_eq!(*number, 50000);
+
+// slices can be allocated as well
+let slice: &mut [u16] = bump_into
+    .alloc_n_with(5, core::iter::repeat(10))
+    .expect("not enough space");
+assert_eq!(slice, &[10; 5]);
+slice[2] = 100;
+assert_eq!(slice, &[10, 10, 100, 10, 10]);
+```
--- src/lib.rs
+++ src/lib.rs
@@ -319,4 +319,29 @@ mod tests {
         assert_eq!(something4, &[71u64][..]);
         assert_eq!(something6, &[]);
     }
+
+    #[test]
+    fn readme_example() {
+        // allocate 64 bytes of uninitialized space on the stack
+        let mut bump_into_space = space!(64);
+        let bump_into = BumpInto::new(&mut bump_into_space[..]);
+
+        // allocating an object produces a mutable reference with
+        // the same lifetime as the `BumpInto` instance, or `None`
+        // if there isn't enough space
+        let number: &mut u64 = bump_into
+            .alloc_with(|| 123)
+            .expect("not enough space");
+        assert_eq!(*number, 123);
+        *number = 50000;
+        assert_eq!(*number, 50000);
+
+        // slices can be allocated as well
+        let slice: &mut [u16] = bump_into
+            .alloc_n_with(5, core::iter::repeat(10))
+            .expect("not enough space");
+        assert_eq!(slice, &[10; 5]);
+        slice[2] = 100;
+        assert_eq!(slice, &[10, 10, 100, 10, 10]);
+    }
 }
