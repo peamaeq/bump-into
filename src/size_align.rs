3c30b73fac6f79e78978ccb32ddd377ec8ee107f
Autumn <autumnontape@tutanota.com>
2020-02-19 14:09:44 -0800

    fix the documentation again

--- src/size_align.rs
+++ src/size_align.rs
@@ -1,7 +1,7 @@
 use core::marker::PhantomData;
 use core::mem;
 
-/// A zero-sized struct that becomes `core::mem::align_of::<T>::()`
+/// A zero-sized struct that becomes `core::mem::size_of::<T>::()`
 /// when converted using `Into<usize>`.
 #[derive(Default, Debug, Copy, Clone)]
 pub struct SizeOf<T> {
